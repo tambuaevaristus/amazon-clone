e3a7258919b73a89668d136dca6fdc717fc4ae42
Tambua Evaristus <evaristustambua@gmail.com>
2022-06-02 01:10:43 +0100

    product design ok...

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,60 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
     <div className="home">
-        <div className="home__container">
-            <img className='home__image'  src="assets/amazon_banner.jpeg"/>
-
-
-        <div className="home__row">
-
-        </div>
+      <div className="home__container">
+        <img className="home__image" src="assets/amazon_banner.jpeg" />
 
         <div className="home__row">
-            
+          <Product
+            title="The learn startup"
+            price="30.32"
+            image="/assets/lean_startup.jpeg"
+            rating={5}
+          />
+          <Product
+            title="The learn startup"
+            price="30.32"
+            image="/assets/lean_startup.jpeg"
+            rating={5}
+          />
         </div>
 
         <div className="home__row">
-            
+          <Product
+            title="The learn startup"
+            price="30.32"
+            image="/assets/lean_startup.jpeg"
+            rating={5}
+          />
+          <Product
+            title="The learn startup"
+            price="30.32"
+            image="/assets/lean_startup.jpeg"
+            rating={5}
+          />
+          <Product
+            title="The learn startup"
+            price="30.32"
+            image="/assets/lean_startup.jpeg"
+            rating={5}
+          />
         </div>
+
+        <div className="home__row">
+          <Product
+            title="The learn startup"
+            price="30.32"
+            image="/assets/lean_startup.jpeg"
+            rating={5}
+          />
         </div>
-        
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
